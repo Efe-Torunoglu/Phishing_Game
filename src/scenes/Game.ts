3feb087da464034a884904cb725f0fd684bcce0d
David Zhang <david8zhang@berkeley.edu>
2024-03-02 17:51:32 -0800

    added simple accept/deny logic

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,33 +1,70 @@
 import Email from '../core/Email'
-import { Constants } from '../utils/Constants'
+import { Constants, EmailData } from '../utils/Constants'
 
-
-// Create Game Class 
+// Create Game Class
 export default class Game extends Phaser.Scene {
+  // Class Variables
+  public email: Email
+  public emailData: EmailData[] = Constants.RAW_EMAIL_DATA
+  public currEmailIndex = 0
+  public numCorrect = 0
+  public numIncorrect = 0
 
-    // Class Variables
-    public email: Email
-    public nextButton: Phaser.GameObjects.Text
-    
-    create() {
+  // Accept & Deny buttons
+  public acceptButton: Phaser.GameObjects.Text
+  public denyButton: Phaser.GameObjects.Text
+  public scoreText: Phaser.GameObjects.Text
 
-        // Shuffle Emails and Initialize Index
-        const emailData = Phaser.Utils.Array.Shuffle(Constants.RAW_EMAIL_DATA)
-        let index = 0
-        
-        // Create Email Object
-        this.email = new Email(this, emailData[index])
-        
-        // Create Button
-        this.nextButton = this.add.text(Constants.WINDOW_WIDTH / 2, Constants.WINDOW_HEIGHT - 100, 'Next')
+  create() {
+    // Create Email Object
+    this.email = new Email(this, this.emailData[this.currEmailIndex])
 
-        // Button Interactivity
-        this.nextButton.setInteractive({ useHandCursor: true}).on(Phaser.Input.Events.POINTER_UP, () => {
+    // Create Button
+    this.acceptButton = this.add
+      .text(
+        Constants.WINDOW_WIDTH / 2 - 100,
+        Constants.WINDOW_HEIGHT - 100,
+        'Accept'
+      )
+      .setOrigin(0, 0.5)
+    this.denyButton = this.add
+      .text(
+        Constants.WINDOW_WIDTH / 2 + 100,
+        Constants.WINDOW_HEIGHT - 100,
+        'Deny'
+      )
+      .setOrigin(0, 0.5)
 
-            // Mod to make sure we dont go out of bounds, update index and point to next email. 
-            index = (index + 1) % emailData.length
-            this.email.setNewEmail(emailData[index])
+    this.scoreText = this.add.text(25, 25, 'Correct: 0\nIncorrect: 0')
 
-        })
+    // Add accept & deny buttons
+    this.acceptButton
+      .setInteractive({ useHandCursor: true })
+      .on(Phaser.Input.Events.POINTER_UP, () => {
+        const currEmailData = this.emailData[this.currEmailIndex]
+        this.handleCorrectness(currEmailData.isReal)
+      })
+
+    this.denyButton
+      .setInteractive({ useHandCursor: true })
+      .on(Phaser.Input.Events.POINTER_UP, () => {
+        const currEmailData = this.emailData[this.currEmailIndex]
+        this.handleCorrectness(!currEmailData.isReal)
+      })
+  }
+
+  handleCorrectness(isCorrect: boolean) {
+    if (isCorrect) {
+      this.numCorrect++
+    } else {
+      this.numIncorrect++
     }
+    this.scoreText.setText(
+      `Correct: ${this.numCorrect}\nIncorrect: ${this.numIncorrect}`
+    )
+
+    // Go to the next email
+    this.currEmailIndex = (this.currEmailIndex + 1) % this.emailData.length
+    this.email.setNewEmail(this.emailData[this.currEmailIndex])
+  }
 }
